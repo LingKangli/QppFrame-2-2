82ce918b07edc523e95bd154ac5e0fc9daeaa151
Kaly <comehere0907@sina.com>
2015-08-26 09:28:16 +0800

    add friend object Of user

---
+++ QppFrame/mode/enity/AsFrindInfo.h
@@ -0,0 +1,17 @@
+//
+//  AsFrindInfo.h
+//  QppFrame
+//
+//  Created by 孙兴国 on 15/8/24.
+//  Copyright (c) 2015年 LingKangLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AsFrindInfo : NSObject
+
+@property(nonatomic)NSString* userName;
+@property(nonatomic)NSString* userEmail;
+@property(nonatomic)NSString* Ssex;
+
+@end
---
+++ QppFrame/mode/enity/AsFrindInfo.m
@@ -0,0 +1,17 @@
+//
+//  AsFrindInfo.m
+//  QppFrame
+//
+//  Created by 孙兴国 on 15/8/24.
+//  Copyright (c) 2015年 LingKangLi. All rights reserved.
+//
+
+#import "AsFrindInfo.h"
+
+@implementation AsFrindInfo
+
+@synthesize userName;
+@synthesize userEmail;
+@synthesize Ssex;
+
+@end
